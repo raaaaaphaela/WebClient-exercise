94b824cb254eb4bbd6be9244036e50e65da04877
Raphaela <raphaela.dohrmann@gmail.com>
2022-12-06 17:14:37 +0100

    Message was missing

---
+++ src/main/java/de/neuefische/webclientexample/elvedinsAPI/Message.java
@@ -0,0 +1,14 @@
+package de.neuefische.webclientexample.elvedinsAPI;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Message {
+
+    private String message;
+
+}
